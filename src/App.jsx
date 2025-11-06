f23c943c31f21f227821cf32fb16a975551927c8
kmh <micic1415@gmail.com>
2025-11-06 17:40:57 +0900

    action6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div>안녕하세요</div>;
+  return (
+    <div>
+      <h3>title</h3>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, id.
+    </div>
+  );
 }
 
 export default App;
